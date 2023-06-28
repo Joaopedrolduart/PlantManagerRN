14fce705165418a5dfa9ad99367d2380c88f572f
Joaopedrolduart <joaopedrolduarte05@gmail.com>
2023-06-28 14:53:57 +0100

    user image

--- src/pages/PlantSelector.tsx
+++ src/pages/PlantSelector.tsx
@@ -1,10 +1,22 @@
 import React from 'react';
-import {View, Text} from 'react-native';
+import {Text, View} from 'react-native';
+import {Header} from '../components/Header';
 
 export function PlantSelector() {
   return (
-    <View>
-      <Text>OLA</Text>
+    <View className="flex-1 space-y-6">
+      <View className="mt-6 space-y-6 px-4">
+        <Header title="João" subtitle="Olá," />
+
+        <View>
+          <Text className="text-base font-medium text-gray-600 dark:text-white">
+            Em qual ambiente
+          </Text>
+          <Text className="text-base text-gray-600 dark:text-white">
+            você quer colocar sua planta?
+          </Text>
+        </View>
+      </View>
     </View>
   );
 }
