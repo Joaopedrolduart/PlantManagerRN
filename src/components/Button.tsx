2915581bd940a676444f2bf1bcfcb9b020b32f62
Joaopedrolduart <joaopedrolduarte05@gmail.com>
2023-07-10 11:15:23 +0100

    Date time picker / date select (android) done

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -11,7 +11,7 @@ export function Button({title, ...props}: ButtonProps) {
   return (
     <StyledComponent component={TouchableOpacity} {...props}>
       <TouchableOpacity
-        className="h-14 items-center justify-center rounded-lg bg-green-600 dark:bg-green-600/75"
+        className="h-14 items-center justify-center rounded-2xl bg-green-600 dark:bg-green-600/75"
         activeOpacity={0.7}
         {...props}>
         <Text className="font-heading text-base text-white ">{title}</Text>
--- src/pages/PlantSave.tsx
+++ src/pages/PlantSave.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {
   Alert,
   Text,
@@ -14,14 +14,43 @@ import waterdrop from '../assets/waterdrop.png';
 import {Button} from '../components/Button';
 import {useRoute} from '@react-navigation/native';
 import {PlantType} from '../@types/PlantType';
+import DateTimePicker, {
+  DateTimePickerEvent,
+} from '@react-native-community/datetimepicker';
+import {format, isBefore} from 'date-fns';
 
 export interface PlantSaveParams {
   plant: PlantType;
 }
 
 export function PlantSave() {
+  const [selectedDateTime, setSelectedDateTime] = useState(new Date());
+  const [showDatePicker, setShowDatePicker] = useState(Platform.OS === 'ios');
   const route = useRoute();
   const {plant} = route.params as PlantSaveParams;
+
+  function handleChangeTime(
+    event: DateTimePickerEvent,
+    dateTime: Date | undefined,
+  ) {
+    if (Platform.OS === 'android') {
+      setShowDatePicker(oldState => !oldState);
+    }
+
+    if (dateTime && isBefore(dateTime, new Date())) {
+      setSelectedDateTime(new Date());
+      return Alert.alert('Escolha uma hora no futuro! ⏰ ');
+    }
+
+    if (dateTime) {
+      setSelectedDateTime(dateTime);
+    }
+  }
+
+  function handleOpenDateTimePicker() {
+    setShowDatePicker(oldstate => !oldstate);
+  }
+
   return (
     <View className="flex-1 justify-between bg-gray-shape">
       <View className="flex-1 items-center justify-center px-8">
@@ -29,15 +58,13 @@ export function PlantSave() {
         <Text className="font-heading text-2xl dark:text-white">
           {plant.name}
         </Text>
-        <Text className="mt-4 text-center font-body text-base">
-          {plant.about}
-        </Text>
+        <Text className="mt-4 text-center font-body ">{plant.about}</Text>
       </View>
 
       <View className="bg-white px-5 pb-5 pt-5">
-        <View className="relative bottom-16 flex-row items-center justify-between rounded-3xl bg-blue-blue_light p-5">
+        <View className="relative bottom-20 flex-row items-center justify-between rounded-3xl bg-blue-blue_light p-5">
           <Image source={waterdrop} className="h-14 w-14" />
-          <Text className="ml-5 flex-1 text-justify font-body text-blue-blue">
+          <Text className="ml-5 flex-1 text-center font-body text-base text-blue-blue">
             {plant.water_tips}
           </Text>
         </View>
@@ -45,6 +72,25 @@ export function PlantSave() {
         <Text className="mb-2 text-center font-heading text-xs text-gray-400">
           Escolha o melhor horário para ser lembrado:
         </Text>
+
+        {showDatePicker && (
+          <DateTimePicker
+            value={selectedDateTime}
+            mode="time"
+            display="spinner"
+            onChange={handleChangeTime}
+          />
+        )}
+
+        {Platform.OS === 'android' && (
+          <TouchableOpacity
+            className="w-full items-center py-10"
+            onPress={handleOpenDateTimePicker}>
+            <Text className="font-heading text-xl">
+              {`Mudar ${format(selectedDateTime, 'HH:mm')}`}
+            </Text>
+          </TouchableOpacity>
+        )}
 
         <Button title="Cadastrar planta" onPress={() => {}} />
       </View>
