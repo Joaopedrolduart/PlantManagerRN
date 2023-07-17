0a196f9cecc9ed961bbaf9ab182b8bdc5f31b259
Joaopedrolduart <joaopedrolduarte05@gmail.com>
2023-07-17 10:37:29 +0100

    fixing stuff

--- src/components/PlantCardPrimary.tsx
+++ src/components/PlantCardPrimary.tsx
@@ -1,9 +1,13 @@
 import React from 'react';
 import {Text} from 'react-native';
-import {RectButtonProps, RectButton} from 'react-native-gesture-handler';
+import {
+  RectButtonProps,
+  RectButton as RNRectButton,
+} from 'react-native-gesture-handler';
 
 import {SvgFromUri} from 'react-native-svg';
 import {tw} from '../lib/tailwind';
+import {styled} from 'nativewind';
 
 interface PlantProps extends RectButtonProps {
   data: {
@@ -12,10 +16,13 @@ interface PlantProps extends RectButtonProps {
   };
 }
 
+const RectButton = styled(RNRectButton);
+
 export const PlantCardPrimary = ({data, ...rest}: PlantProps) => {
   return (
     <RectButton
-      style={tw`rounded-3xl m-2 flex-1 items-center space-y-2 bg-gray-100 py-8  dark:bg-green-500/40`}
+      style={tw`rounded-3xl`}
+      className="m-2 flex-1 items-center space-y-2 bg-gray-100 py-8  dark:bg-green-500/40"
       {...rest}>
       <SvgFromUri uri={data.photo} width={70} height={70} />
       <Text className="font-heading text-green-600"> {data.name} </Text>
