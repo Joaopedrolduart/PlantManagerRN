d5f7a1c6ad60c9e2be30b6f6477486e652fa1ff0
Joaopedrolduart <joaopedrolduarte05@gmail.com>
2023-07-17 15:09:53 +0100

    fix Myplants (nextWatered) description

--- App.tsx
+++ App.tsx
@@ -4,6 +4,7 @@ import {GestureHandlerRootView} from 'react-native-gesture-handler';
 
 export default function App() {
   return (
+    // eslint-disable-next-line react-native/no-inline-styles
     <GestureHandlerRootView style={{flex: 1}}>
       <Routes />
     </GestureHandlerRootView>
